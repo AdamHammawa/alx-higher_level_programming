a35a8618f08e345a72d31bdd197af848b010c1df
AdamHammawa <adamsani2020@gmail.com>
2022-09-06 15:34:44 -0700

    show create

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
--- Write a script that prints the full description of the table first_table from the database hbtn_0c_0
+-- script that prints the full description of the table first_table from the database hbtn_0c_0
 -- The database name will be passed as an argument of the mysql command
 -- You are not allowed to use the DESCRIBE or EXPLAIN statements
-SHOW TABLE first_table;
+SHOW CREATE TABLE first_table;
