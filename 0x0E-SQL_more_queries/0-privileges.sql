02dcc660eee3b00c04354ade6eadcaa89f31a0f9
AdamHammawa <adamsani2020@gmail.com>
2022-09-07 14:19:00 -0700

    show grant

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
---a script that lists all privileges of the MySQL users
--- the user name is user_0d_1 and user_0d_2 
+-- List all privileges for users 'user_0d_1' and 'user_0d_2'
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
